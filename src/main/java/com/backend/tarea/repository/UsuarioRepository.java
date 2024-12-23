b3fb5d6ba6a427416cab923f193d14e7d3427237
Andres <yanezp006@gmail.com>
2024-12-22 23:18:05 -0500

    modificacion yanez

--- src/main/java/com/backend/tarea/repository/UsuarioRepository.java
+++ src/main/java/com/backend/tarea/repository/UsuarioRepository.java
@@ -1,4 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
 package com.backend.tarea.repository;
+
 
 import com.backend.tarea.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
