cd93d4cfe64087abdcc7b7c13a8e1efd9072ece3
CristianRuiz21 <sidodi17@gmail.com>
2024-12-22 23:02:14 -0500

    Quinta subida

---
+++ src/main/java/com/backend/tarea/repository/ReservaRepository.java
@@ -0,0 +1,8 @@
+package com.backend.tarea.repository;
+
+import com.backend.tarea.model.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    // Puedes añadir métodos personalizados si es necesario
+}
