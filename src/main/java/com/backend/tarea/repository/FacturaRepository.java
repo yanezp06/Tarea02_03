065eb15ed3dc8a92ba787bc80078ecb6e38d4f8b
CristianRuiz21 <sidodi17@gmail.com>
2024-12-22 23:03:09 -0500

    Sexta subida

---
+++ src/main/java/com/backend/tarea/repository/FacturaRepository.java
@@ -0,0 +1,8 @@
+package com.backend.tarea.repository;
+
+import com.backend.tarea.model.Factura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacturaRepository extends JpaRepository<Factura, Long> {
+    // Puedes añadir métodos personalizados si es necesario
+}
